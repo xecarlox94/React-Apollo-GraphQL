a9c6f3d8f300358e4a23f97c756ed351d22ae4f5
xecarlox94 <jf94.uk@gmail.com>
2020-01-07 17:28:12 +0000

    graphql server setup

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const GraphQLHTTP = require("express-graphql");
+
+const schema = require("./schema");
+const app = express();
+
+app.use(
+  "/graphql",
+  GraphQLHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log("server running!"));
